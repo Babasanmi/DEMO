5e9bdd626af0c77d572efa8ea7e8be9153cb5666
Jeremiah Babasanmi <jsbabasanmi@gmail.com>
2018-04-12 17:01:04 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <style>
+    body {
+    position: absolute;
+    width: 748px;
+    height: 434px;
+    background-color: #56F2CD;
+    margin: 50px;
+}
+
+p1 {
+    position: absolute;
+    width: 431px;
+    height: 44px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    line-height: normal;
+    font-size: 18px;
+    color: #000000;
+}
+
+p2 {
+    position: absolute;
+    width: 404px;
+    height: 43px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    line-height: normal;
+    font-size: 18px;
+    color: #000000;
+}
+
+p {
+    position: absolute;
+    width: 499px;
+    height: 33px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    line-height: normal;
+    font-size: 18px;
+    color: #000000;
+} 
+</style>
+
+</head>
+<body>
+    <div>  <p1>  Hello Guys, My Name is Jeremiah Babasanmi</p1> </br>
+           <p2>My Very Basic Figma Page *dont laugh* </p2> </br>
+           <p>You are reading this text at exactly <span id="date"><?php echo date("d/m/Y") ?></span> at exactly <span id="time"><?php echo date("h:i:sa"); ?></span> in Nigeria.</p>
+
+    </div>
+</body>
+</html>
